a9a7c4bd14f838912965c3674ce7ecbe3c954858
KMINALI2005 <162717637+KMINALI2005@users.noreply.github.com>
2025-08-02 13:19:30 -0400

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,57 +1,40 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-fun getLocalProperty(key: String, project: org.gradle.api.Project): String {
-    val properties = java.util.Properties()
-    val localPropertiesFile = project.rootProject.file("local.properties")
-    if (localPropertiesFile.exists()) {
-        properties.load(java.io.FileInputStream(localPropertiesFile))
-        return properties.getProperty(key) ?: ""
-    }
-    return ""
-}
-
-android {
+android {    
     namespace = "com.example.debt_manager"
     compileSdk = 34
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
-    sourceSets {
-        getByName("main").java.srcDirs("src/main/kotlin")
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
-        applicationId = "com.example.debt_manager"
-        minSdk = 21
-        targetSdk = 34
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.my_apmanp"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
-    signingConfigs {
-        create("release") {
-            keyAlias = System.getenv("CM_KEY_ALIAS") ?: getLocalProperty("keyAlias", project)
-            keyPassword = System.getenv("CM_KEY_PASSWORD") ?: getLocalProperty("keyPassword", project)
-            storeFile = System.getenv("CM_KEYSTORE_PATH")?.let { project.file(it) } ?: project.file(getLocalProperty("storeFile", project))
-            storePassword = System.getenv("CM_KEYSTORE_PASSWORD") ?: getLocalProperty("storePassword", project)
-        }
-    }
-
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("release")
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
