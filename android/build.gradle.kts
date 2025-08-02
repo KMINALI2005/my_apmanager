523933c9e103164ee31792d450d78a4cd4d7de9c
KMINALI2005 <162717637+KMINALI2005@users.noreply.github.com>
2025-08-02 13:11:34 -0400

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,32 +1,6 @@
-buildscript {
-    ext.kotlin_version = '1.9.23' // تم تحديث إصدار Kotlin
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        // تم تحديث إصدار Android Gradle Plugin ليتوافق مع Gradle 8.9
-        classpath 'com.android.tools.build:gradle:8.5.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.buildDir = '../build'
-subprojects {
-    project.buildDir = "${rootProject.buildDir}/${project.name}"
-}
-subprojects {
-    project.evaluationDependsOn(':app')
-}
-
-tasks.register("clean", Delete) {
-    delete rootProject.buildDir
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
